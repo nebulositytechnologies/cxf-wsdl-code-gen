d55b827fb9c9f7053b2b113114f027344f056c94
Vijay Akkineni <akkineni.vijay@gmail.com>
2023-03-15 19:17:39 -0400

    renamed package

--- src/main/java/com/nebulosity/Main.java
+++ src/main/java/com/nebulosity/Main.java
@@ -1,9 +1,9 @@
-package org.example;
+package com.nebulosity;
 
 import com.appriss.methcheck.services.*;
+import java.util.List;
 
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello CXF CodeGen");
   }
 }
